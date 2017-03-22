81aa4ffb8d9d03d2ba85d8b57e4204f79a7a79d8
Nazma Kotcherla <nazmas7@gmail.com>
2017-03-22 12:59:07 -0500

    Implemented DAG

--- hooks_impl.php
+++ hooks_impl.php
@@ -8,29 +8,52 @@ define('RC_YES', 1);
 define('RC_NO', 0);
 define('RC_FORM_COMPLETE', 2);
 
+
+function shutDownFunction(){
+	$error = error_get_last();
+        error_log(print_r($error,true));
+
+}
+
+
 function reseval_save_record($project_id, $record, $instrument, $event_id,
                                    $group_id, $survey_hash, $response_id)
 {
     // Provides access to REDCap helper functions and database connection.
     //print "hello";
+    register_shutdown_function('shutDownFunction');
     error_log("**********************************ENTERED RESEVAL_SAVE FUNCTION*****************************");
     global $conn; // REDCapism
     require_once(REDCAP_ROOT.'redcap_connect.php');
 
     // Load configuration plugin configuration.
     define('FRAMEWORK_ROOT', REDCAP_ROOT.'plugins/framework/');
-    require_once(FRAMEWORK_ROOT.'/PluginConfig.php');
+    error_log('test 1');
+    require_once(FRAMEWORK_ROOT.'PluginConfig.php');
+    
+    error_log('test 2');
+    require_once(FRAMEWORK_ROOT.'ProjectModel.php');
+    error_log('test 3');
+
     $CONFIG = new PluginConfig(dirname(__FILE__).'/reseval.ini');
     //error_log("inside config");
     //error_log($CONFIG['resident_info_pid']);
     // This differs from REDCap's Record class in that project records can be
     // queried for by fields other than record id.
-   require_once(dirname(__FILE__).'/../utils/records.php');
+     require_once(dirname(__FILE__).'/../utils/records.php');
 
    // Evaluates REDCap branching logic syntax.
     require_once(APP_PATH_DOCROOT.'Classes/LogicTester.php');
     // Provides properly formated REDCap record data for use with LogicTester.
     require_once(APP_PATH_DOCROOT.'Classes/Records.php');
+
+     error_log('test 4');
+
+    $residents =  new ProjectModel($project_id, $conn);
+
+    error_log("this is the new residents object");
+   
+    error_log(print_r($residents, true));
     
     $survey_instruments = array($CONFIG['res_eval_link'] => $CONFIG['res_eval_by_fac'],$CONFIG['fac_eval_link'] => $CONFIG['fac_eval_by_res']);
     
@@ -73,9 +96,18 @@ function reseval_save_record($project_id, $record, $instrument, $event_id,
 
     }
   
+  
     $record_data = Records::getData('array', $record);
-                       
+  
+    $result_save_DAG = save_DAG_for_record( $project_id,
+					    $record_data,
+                                            $record_data[$record][$event_id]['institution']);   
+  
+    //$record_data = Records::getData('array', $record);
+
+                   
     $result_res_email = get_and_save_emails(
+                                         $residents,
 		  			 $record,
 		  			 $CONFIG,
 		  			 $record_data,
@@ -91,6 +123,7 @@ function reseval_save_record($project_id, $record, $instrument, $event_id,
     }
 
    $result_eval_email =  get_and_save_emails(
+					 $residents,
                   			 $record,
                    		         $CONFIG,
                   			 $record_data,
@@ -107,14 +140,28 @@ function reseval_save_record($project_id, $record, $instrument, $event_id,
         error_log('Failed in saving the faculty email in invite information');
     }
 
+    
+    $current_time = date("H:i:s");
+    $result_save_time = save_value_in_record( $record,
+					      $CONFIG,
+					      $current_time,
+					      $CONFIG['submit_time']);
 
-   error_log("reached the end of the program bye bye");
-   return true;
+    $current_date = date("m/d/Y");
+    $result_save_date = save_value_in_record( $record,
+                                              $CONFIG,
+                                              $current_date,
+                                              $CONFIG['submit_date']);
+
+
+
+    error_log("reached the end of the program bye bye");
+    return true;
 
 }
 
 // function that is specific to Resident evaluation project that gets the email addresses and saves in other instrument 
-function get_and_save_emails($record, $CONFIG, $record_data,$institutions,$invite_email_label,$proj_num,$info_email_field,$event_id, $conn){
+function get_and_save_emails($residents,$record, $CONFIG, $record_data,$institutions,$invite_email_label,$proj_num,$info_email_field,$event_id, $conn){
  
   $get_inst_value = array();
  
@@ -127,7 +174,16 @@ function get_and_save_emails($record, $CONFIG, $record_data,$institutions,$invit
    $rec_num = max($get_inst_value);
 
    $rec_data =  get_record_data($rec_num, $proj_num, $conn);
-        
+     
+
+   error_log("am i reaching here");
+
+//   $rec_data = $residents->get_record_by('record',$rec_num);
+ 
+//** By using the ProjectModel, there is no method in it that takes in the record number, project number and returns the record_data. All the functions in the framework are restricted to the current project.
+
+
+   error_log(print_r ($rec_data , true)); 
    $result_save_emails =  save_value_in_record($record,
          			               $CONFIG,
 				  	       $rec_data[$info_email_field],
@@ -148,6 +204,9 @@ function get_and_save_emails($record, $CONFIG, $record_data,$institutions,$invit
 
 function save_value_in_record($record,$CONFIG,$rec_value,$rec_field){
 
+
+//require_once(REDCAP_ROOT.'redcap_connect.php');
+
   error_log("inside save value function");
   error_log($CONFIG['proj_token']);
   error_log($rec_value);
@@ -160,6 +219,11 @@ function save_value_in_record($record,$CONFIG,$rec_value,$rec_field){
         'field_name' => $rec_field,
         'value' => $rec_value
     ));
+
+
+//   $response = REDCap::saveData(180,'json',json_encode($field_val),'normal','MDY','eav','amc');
+//   error_log (print_r($response, true));
+
     list($success, $error_msg) = save_redcap_data(
         $CONFIG['api_url'],
         $CONFIG['proj_token'],
@@ -175,5 +239,19 @@ function save_value_in_record($record,$CONFIG,$rec_value,$rec_field){
 
 
 }
+
+function save_DAG_for_record( $project_id,$record_data,$DAG_name)
+{
+
+   $response = REDCap::saveData($project_id,'array',$record_data,'normal','MDY','eav',$DAG_name);
+   error_log (print_r($response, true));
+
+
+}
+
+
+
+
+
 
 ?>
